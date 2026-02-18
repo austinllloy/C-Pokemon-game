7efb3f0834d93dc166e38c2a2193f9928e055924
Austin Loyd <bigdan680@gmail.com>
2026-02-18 01:19:02 -0800

    Update PokemonPiece.cpp

--- PokemonPiece.cpp
+++ PokemonPiece.cpp
@@ -1,9 +1,4 @@
-/*
-   Name: Austin Loyd, 2002553501, Assignment 5, 0
-Description: Prints to log hello code grade.
-Input: EXPECTED_PROGRAM_INPUT.
-Output: Class output.
-*/
+
 #include "PokemonPiece.h"
 #include "LegendaryType.h"
 #include "TankType.h"
@@ -115,3 +110,4 @@ cout << endl;
     } 
     
     
+
